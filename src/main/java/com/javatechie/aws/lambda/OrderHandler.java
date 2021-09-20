99163649a0e7c0a70451d599a1f3fb33b9e20641
vn51sci <vn51sci@homeoffice.wal-mart.com>
2021-09-20 11:30:19 +0530

    aws lambad

---
+++ src/main/java/com/javatechie/aws/lambda/OrderHandler.java
@@ -0,0 +1,9 @@
+package com.javatechie.aws.lambda;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+
+public class OrderHandler extends SpringBootRequestHandler<APIGatewayProxyRequestEvent, Object> {
+	//helps to serialize and deserialize object
+}
